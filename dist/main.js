9bb761826b0821fd043a15c4d01d62bb947cf9dc
gefgu <gefgu@hotmail.com>
2022-03-29 20:03:19 -0300

    Add hide/reveal of dropdown elements using css

--- dist/main.js
+++ dist/main.js
@@ -2,12 +2,26 @@
   const dropdownContainerClassName = "dropdown-container";
   const dropdownItemClassName = "dropdown-item";
   const dropdownIconClassName = "dropdown-icon";
+  const openMenuClass = "open-menu";
   const dropdownsContainerList = document.querySelectorAll(
     `.${dropdownContainerClassName}`
   );
   dropdownsContainerList.forEach((dropdownContainer) => {
     dropdownContainer.addEventListener("click", () => {
-      dropdownContainer.classList.toggle("open-menu");
+      dropdownContainer.classList.toggle(`${openMenuClass}`);
     });
   });
+
+  // Apply styles
+
+  const style = document.createElement("style");
+  document.head.appendChild(style);
+  style.sheet.insertRule(`
+  .${dropdownContainerClassName} > .${dropdownItemClassName} {
+    display: none;
+  }`);
+  style.sheet.insertRule(`
+  .${dropdownContainerClassName}.${openMenuClass} > .${dropdownItemClassName} {
+    display: block;
+  }`);
 })();
