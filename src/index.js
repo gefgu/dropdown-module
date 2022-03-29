6796fd740e891935ac57b10161142cf5908e6e5a
gefgu <gefgu@hotmail.com>
2022-03-29 20:09:38 -0300

    Move javascript to src file and add css linking

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import "./style.css";
+
+(() => {
+  const dropdownContainerClassName = "dropdown-container";
+  const dropdownItemClassName = "dropdown-item";
+  const dropdownIconClassName = "dropdown-icon";
+  const openMenuClass = "open-menu";
+  const dropdownsContainerList = document.querySelectorAll(
+    `.${dropdownContainerClassName}`
+  );
+  dropdownsContainerList.forEach((dropdownContainer) => {
+    const button = dropdownContainer.querySelector(`.${dropdownIconClassName}`);
+    button.addEventListener("click", () => {
+      dropdownContainer.classList.toggle(`${openMenuClass}`);
+    });
+  });
+
+  const style = document.createElement("style");
+  document.head.appendChild(style);
+  style.sheet.insertRule(`
+  .${dropdownContainerClassName} > .${dropdownItemClassName} {
+    display: none;
+  }`);
+  style.sheet.insertRule(`
+  .${dropdownContainerClassName}.${openMenuClass} > .${dropdownItemClassName} {
+    display: block;
+  }`);
+})();
