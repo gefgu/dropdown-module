a2595888114ae6a4c40f24c075194c3145975df2
gefgu <gefgu@hotmail.com>
2022-03-29 19:52:14 -0300

    Add eslint and prettier integration and class names in js file

---
+++ main.js
@@ -0,0 +1,8 @@
+(() => {
+  const dropdownContainerClassName = "dropdown-container";
+  const dropdownItemClassName = "dropdown-item";
+  const dropdownIconClassName = "dropdown-icon";
+  const dropdownsContainers = document.querySelectorAll(
+    `.${dropdownContainerClassName}`
+  );
+})();
